dc44b1c5d1384f4ff4b6d9b2abf42c75c1821c8c
reSHARMA <reshabhsh@gmail.com>
2025-01-23 01:15:37 -0800

    add option to run web ui or in terminal

--- agent.py
+++ agent.py
@@ -1,6 +1,7 @@
 from autogen_core.models import ChatCompletionClient
 from autogen_agentchat.agents import AssistantAgent, UserProxyAgent
 from autogen_agentchat.teams import SelectorGroupChat
+from autogen_agentchat.ui import Console
 from autogen_agentchat.conditions import TextMentionTermination
 from autogen_agentchat.messages import AgentEvent, ChatMessage
 
@@ -14,15 +15,18 @@ from datetime import datetime, timedelta
 
 import streamlit as st
 
+# Conditional import for Streamlit
+USE_STREAMLIT = False
+
 policy_system = PolicySystem()
 user_input = ""
 
-if 'messages' not in st.session_state:
-    st.session_state['messages'] = []
+if USE_STREAMLIT:
+    if 'messages' not in st.session_state:
+        st.session_state['messages'] = []
 
-if 'policies' not in st.session_state:
-    st.session_state['policies'] = []
-
+    if 'policies' not in st.session_state:
+        st.session_state['policies'] = []
 
 async def main() -> None:
     # Register CalendarAPI with the policy system
@@ -38,19 +42,18 @@ async def main() -> None:
 
     model_client = ChatCompletionClient.load_component(config)
 
-    # Function to capture user input
     def web_input_func(prompt: str) -> str:
-        global user_input
-        user_input = prompt
-        return user_input
-    
-    def web_input_func1(prompt: str) -> str:
         global user_input
         temp = user_input
         user_input = ""
         return temp 
 
-    user = UserProxyAgent("User", input_func=web_input_func1) 
+    user = None
+
+    if USE_STREAMLIT:
+        user = UserProxyAgent("User", input_func=web_input_func) 
+    else:
+        user = UserProxyAgent("User", input_func=input)
 
     # User: The user who is interacting with you. Must be asked for confirmation for irreversible tasks or when there are options available.
     planner = AssistantAgent(
@@ -216,7 +219,8 @@ async def main() -> None:
     calendar = AssistantAgent(
         name="Calendar",
         system_message="""
-        You are a calendar app with access to calendar APIs as tools, you will be given a request to fulfill. Call the tools available to you to fulfill the request. Asume offset-naive datetime for simplicity.
+        You are a calendar agent with access to calendar APIs as tools, you will be given a request to fulfill.
+        Call the tools available to you to fulfill the request. Asume offset-naive datetime for simplicity.
     """,
         tools=[calendar_reserve, calendar_read, calendar_check_availability],
         model_client=model_client
@@ -270,7 +274,9 @@ async def main() -> None:
     expedia = AssistantAgent(
         name="Expedia",
         system_message="""
-        You are an Expedia app with access to Expedia APIs as tools, you will be given a request to fulfill. Call the tools available to you to fulfill the request.
+        You are an Expedia app (a travel and experience booking app) with access to Expedia APIs as tools, you will be given a request to fulfill.
+        Call the tools available to you to fulfill the request.
+        Asume offset-naive datetime for simplicity.
     """,
         tools=[expedia_search_flights, expedia_book_hotel, expedia_rent_car, expedia_book_experience, expedia_book_cruise, expedia_search_hotels, expedia_search_rental_cars, expedia_search_experience, expedia_search_cruise],
         model_client=model_client
@@ -279,35 +285,39 @@ async def main() -> None:
     termination = TextMentionTermination("terminate")
 
     def selector_func(messages: Sequence[AgentEvent | ChatMessage]) -> str | None:
-        if len(messages) == 0:
-            messages = st.session_state['messages'] 
+        if USE_STREAMLIT:
+            if len(messages) == 0:
+                messages = st.session_state['messages'] 
 
-        print(f"Debug: Number of messages received: {len(messages)}")
-        print("****", messages)
+            print(f"Debug: Number of messages received: {len(messages)}")
+            print("****", messages)
 
-        if len(messages) > 0:
-            name = messages[-1].source
-            print("*******", name)
-            avatar = "🤖"
-            if name == "User":
-                avatar = "🧑‍💼"
-            if name == "Permission":
-                avatar = "👮‍♀️"
-            if name == "Planner":
-                avatar = "🤔"
-    
-            message = st.chat_message(name=name, avatar=avatar)
+            if len(messages) > 0:
+                name = messages[-1].source
+                print("*******", name)
+                avatar = "🤖"
+                if name == "User":
+                    avatar = "🧑‍💼"
+                if name == "Permission":
+                    avatar = "👮‍♀️"
+                if name == "Planner":
+                    avatar = "🤔"
+        
+                message = st.chat_message(name=name, avatar=avatar)
 
-            text = messages[-1].content
-            message.write(text)
+                text = messages[-1].content
+                message.write(text)
 
         agent = ""
 
         if len(messages) == 0:
             print("Debug: No messages, waiting for user input.")
-            user_input = st.chat_input("Say something")
-            print(f"Debug: User input received: {user_input}")
-            web_input_func(user_input)
+
+            if USE_STREAMLIT:
+                user_input = st.chat_input("Say something")
+                print(f"Debug: User input received: {user_input}")
+                web_input_func(user_input)
+
             agent = "User"
         
         if len(messages) == 1: 
@@ -325,12 +335,14 @@ async def main() -> None:
             for message in messages:
                 if message.source == "Permission" and message.type == "ToolCallRequestEvent":
                     for msg in message.content:
-                        st.session_state["policies"].append(json.loads(msg.arguments)['code'])
+                        if USE_STREAMLIT:
+                            st.session_state["policies"].append(json.loads(msg.arguments)['code'])
             
-            with st.sidebar:
-                st.header("Policies")
-                for policy in st.session_state["policies"]:
-                    st.code(pprint.pformat(policy, width=40), language="python")
+            if USE_STREAMLIT:
+                with st.sidebar:
+                    st.header("Policies")
+                    for policy in st.session_state["policies"]:
+                        st.code(pprint.pformat(policy, width=40), language="python")
 
             messages = [message for message in messages if message.source != "Permission"]
             agent = "Planner"
@@ -339,10 +351,6 @@ async def main() -> None:
             next_agent = messages[-1].content.split(":")[0]
             print(f"Debug: Last message from 'Planner', next agent determined as: {next_agent}")
             agent =  next_agent
-
-            # if agent == "User":
-            #     prompt = st.chat_input("Say something", key="updates")
-            #     web_input_func(prompt)
 
             if agent == "terminate":
                 agent = "Planner"
@@ -361,7 +369,10 @@ async def main() -> None:
     task += "Today's date is 15th Jan 2025"
 
     stream = groupchat.run_stream()
-    async for _ in stream:
-        pass
+    if USE_STREAMLIT:
+        async for _ in stream:
+            pass
+    else:
+        await Console(stream)
 
 asyncio.run(main())

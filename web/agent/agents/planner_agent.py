b0481b9f22abeb1644fe2cfda8e3a7428bf42481
reSHARMA <reshabhsh@gmail.com>
2025-05-09 02:39:01 -0700

    Refine planner agent system message for improved clarity and guidance. Correct spelling errors and enhance instructions on application invocation, emphasizing the preference for non-user applications to minimize human intervention.

--- web/agent/agents/planner_agent.py
+++ web/agent/agents/planner_agent.py
@@ -15,17 +15,18 @@ class PlannerAgent(BaseAgent):
         Args:
             model_client: The model client to use
         """
-        system_message = f"""You have access to multiple different applications which you must invoke to complete the user reuqest. Output the name of the application from the application given to you which must be invoked next. You will see the history of applications invoked and their results. Along with the name of the application, send a description of the task that application needs to perform with all the necessary data you have without explicitly sending the exact user request. Only send the necessary information.
+        system_message = f"""You have access to multiple different applications which you must invoke to complete the user request. Output the name of the application from the applications given to you which must be invoked next. You will see the history of applications invoked and their results. Along with the name of the application, send a description of the task that application needs to perform with all the necessary data you have without explicitly sending the exact user request. Only send the necessary information.
 
         List of the available application with description:
         Calendar: A calendar app with API to reserve, check availability and read the calendar data.
         Expedia: A travel booking application with APIs for searching, booking and paying for flights, hotels, rental cars, experiences like cruises.
-        Wallet: A wallet application with saved cards and with APIs for adding, removing, updating and getting credit card information for payment.
+        Wallet: A wallet application with saved cards and with APIs for adding, removing, updating and getting credit card information.
         ContactManager: A contact manager application with APIs to add, remove, update and get contact information.
         User: The user application only for asking the user for input and data.
 
         Think deeply and break the task into sub tasks for the application.
         First output the name of the application and then the description in the format, application: description. The description must contains all the required information for the application, do not make up data, if you need data invoke the User application to get the required data first before calling the application.
+        Prefer invoking applications other than User application to reduce the human in the loop.
         Only output one application and the description of the task for that application.
 
         When all tasks are completed from your end, output terminate along with the reason of termination.

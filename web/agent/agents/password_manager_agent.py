c680b808327fe0364f507d8a0ed019241880e166
reSHARMA <reshabhsh@gmail.com>
2025-05-19 05:44:45 +0000

    Refactor PasswordManagerAgent method signatures to utilize Annotated types for improved clarity and documentation of parameters, and streamline system message for user interaction.

--- web/agent/agents/password_manager_agent.py
+++ web/agent/agents/password_manager_agent.py
@@ -6,6 +6,7 @@ from src.policy_system.api_annotation import APIAnnotationBase
 from src.utils.attribute_tree import AttributeTree
 from src.utils.dummy_data import generate_dummy_data
 from config import WILDCARD
+from typing import Annotated
 
 # Set up logging
 logger = logging.getLogger(__name__)
@@ -136,52 +137,7 @@ class PasswordManagerAgent(BaseAgent):
         system_message = """
         You are a password manager agent, you manage the passwords stored by the user. The user can ask you for the password for a specific website or service (e.g. gmail, facebook, twitter, etc. as service name) and each website or service can have multiple user names (e.g. xyz@gmail.com, user123, @Ron, etc. as user name). Use the tools provided to you to complete the task given to you. Start with reasoning about the task and then use the tools to complete the task.
 
-        List of tools available to you
-        [
-            list_all_saved_password_services,
-            list_all_saved_password_users,
-            get_password,
-            add_password,
-            remove_password,
-            update_password
-        ]
-        
-        ## Description of the tools available to you
-
-        Use the tool `list_all_saved_password_services` to get all the saved password services. This tool returns a list of all the saved password services and does not take any parameters. Example output: ["gmail", "facebook", "twitter"].
-        Should be used ONLY when you need to see all available services that have saved passwords.
-
-        Use the tool `list_all_saved_password_users` to get all the saved password users for the given service name. The tool takes the following parameters:
-        - service_name: The name of the service to get the password for, example "gmail".
-        and returns a list of all the saved password users for the given service name. Example output: ["xyz@gmail.com", "user123", "@Ron"].
-        Should be used when you need to see all users that have saved passwords for a specific service. If you have a specific service name, always use this tool directly instead of listing all services.
-
-        Use the tool `get_password` to get the password for the given service name and user name. The tool takes the following parameters:
-        - service_name: The name of the service to get the password for, example "gmail".
-        - user_name: The name of the user to get the password for, example "xyz@gmail.com".
-        and returns the password for the given service name and user name. 
-        Should be used when you need to get the password for a specific service and user.
-
-        Use the tool `add_password` to add a password to the password manager. The tool takes the following parameters:
-        - service_name: The name of the service to add the password for, example "gmail".
-        - user_name: The name of the user to add the password for, example "xyz@gmail.com".
-        - password: The password to add, example "1234567890".
-        Should be used when you need to add a password for a specific service and user.
-
-        Use the tool `remove_password` to remove a password from the password manager. The tool takes the following parameters:
-        - service_name: The name of the service to remove the password for, example "gmail".
-        - user_name: The name of the user to remove the password for, example "xyz@gmail.com".
-        Should be used when you need to remove a password for a specific service and user.
-
-        Use the tool `update_password` to update a password in the password manager. The tool takes the following parameters:
-        - service_name: The name of the service to update the password for, example "gmail".
-        - user_name: The name of the user to update the password for, example "xyz@gmail.com".
-        - password: The password to update, example "1234567890".
-        Should be used when you need to update a password for a specific service and user.
-
         Carefully pick the tool to use based on the user request and you can use multiple tools if needed. If you have a specific service name, use the appropriate tool directly instead of listing all services first.
-
-        Return "done" when you have completed your work.
         """
         
         policy_system.register_api(PasswordManagerAPI)
@@ -200,91 +156,37 @@ class PasswordManagerAgent(BaseAgent):
         super().__init__("PasswordManager", system_message, tools, model_client)
         
     async def password_manager_list_all_saved_password_services(self) -> str:
-        """
-        Get all the saved password services
-
-        Args:
-            None
-            
-        Returns:
-            The result of the operation
-        """
+        """Get all the saved password services, example output: ["gmail", "facebook", "twitter"]"""
         logger.info(f"Calling PasswordManagerAPI list_all_saved_password_services")
         result = self.password_manager_api.list_all_saved_password_services()
         return result
         
-    async def password_manager_list_all_saved_password_users(self, service_name: str) -> str:
-        """
-        Get all the saved password users for the given service name
-        
-        Args:
-            service_name: The name of the service to get the password for, example "gmail".
-            
-        Returns:
-            The result of the operation
-        """
+    async def password_manager_list_all_saved_password_users(self, service_name: Annotated[str, "service name, example: gmail"]) -> str:
+        """Get all the saved password users for the given service name, example output: ["xyz@gmail.com", "user123", "@Ron"] for service name "gmail" """
         logger.info(f"Calling PasswordManagerAPI list_all_saved_password_users with service_name={service_name}")
         result = self.password_manager_api.list_all_saved_password_users(service_name=service_name)
         return result
         
-    async def password_manager_add_password(self, service_name: str, user_name: str, password: str) -> str:
-        """
-        Add a password to the password manager
-        
-        Args:
-            service_name: The name of the service to add the password for, example "gmail".
-            user_name: The name of the user to add the password for, example "xyz@gmail.com".
-            password: The password to add, example "1234567890".
-            
-        Returns:
-            The result of the operation
-        """
+    async def password_manager_add_password(self, service_name: Annotated[str, "service name, example: gmail"], user_name: Annotated[str, "user name, example: xyz@gmail.com"], password: Annotated[str, "password, example: 1234567890"]) -> str:
+        """Add a password to the password manager"""
         logger.info(f"Calling PasswordManagerAPI add_password with service_name={service_name}, user_name={user_name}, password={password}")
         result = self.password_manager_api.add_password(service_name=service_name, user_name=user_name, password=password)
         return result
         
-    async def password_manager_remove_password(self, service_name: str, user_name: str) -> str:
-        """
-        Remove a password from the password manager
-        
-        Args:
-            service_name: The name of the service to remove the password for, example "gmail".
-            user_name: The name of the user to remove the password for, example "xyz@gmail.com".
-            
-        Returns:
-            The result of the operation
-        """
+    async def password_manager_remove_password(self, service_name: Annotated[str, "service name, example: gmail"], user_name: Annotated[str, "user name, example: xyz@gmail.com"]) -> str:
+        """Remove a password from the password manager"""
         logger.info(f"Calling PasswordManagerAPI remove_password with service_name={service_name}, user_name={user_name}")
         result = self.password_manager_api.remove_password(service_name=service_name, user_name=user_name)
-        return result 
+        return result
 
-    async def password_manager_update_password(self, service_name: str, user_name: str, password: str) -> str:
-        """
-        Update a password in the password manager
-        
-        Args:
-            service_name: The name of the service to update the password for, example "gmail".
-            user_name: The name of the user to update the password for, example "xyz@gmail.com".
-            password: The password to update, example "1234567890".
-            
-        Returns:
-            The result of the operation
-        """
+    async def password_manager_update_password(self, service_name: Annotated[str, "service name, example: gmail"], user_name: Annotated[str, "user name, example: xyz@gmail.com"], password: Annotated[str, "password, example: 1234567890"]) -> str:
+        """Update a password in the password manager"""
         logger.info(f"Calling PasswordManagerAPI update_password with service_name={service_name}, user_name={user_name}, password={password}")
         result = self.password_manager_api.update_password(service_name=service_name, user_name=user_name, password=password)
         return result   
     
-    async def password_manager_get_password(self, service_name: str, user_name: str) -> str:
-        """
-        Get the password for the given service name and user name
-        
-        Args:
-            service_name: The name of the service to get the password for, example "gmail".
-            user_name: The name of the user to get the password for, example "xyz@gmail.com".
-            
-        Returns:
-            The result of the operation
-        """
+    async def password_manager_get_password(self, service_name: Annotated[str, "service name, example: gmail"], user_name: Annotated[str, "user name, example: xyz@gmail.com"]) -> str:
+        """Get the password for the given service name and user name"""
         logger.info(f"Calling PasswordManagerAPI get_password with service_name={service_name}, user_name={user_name}")
         result = self.password_manager_api.get_password(service_name=service_name, user_name=user_name)
         return result
